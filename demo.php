f7be93b7516e834a6e1371e7618cdaf322fb1389
idembele70 <idembele70@gmail.com>
2021-03-02 19:26:33 +0100

    ok

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+$classe = [
+    [
+        'nom' => 'Doe',
+        'prenom' => 'Jean',
+        'notes' => [16, 17, 18]
+    ],
+    [
+        'nom' => 'mari',
+        'prenom' => 'renard',
+        'notes' => [6, 3, 5]
+    ]
+];
+print_r($classe[1]["notes"]);
