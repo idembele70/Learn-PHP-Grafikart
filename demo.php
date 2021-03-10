fe34ac69401cca6d78765b3708dc575f379e3682
idembele70 <idembele70@gmail.com>
2021-03-10 18:30:20 +0100

    boucles

--- demo.php
+++ demo.php
@@ -1,13 +1,22 @@
 <?php
-$horaire = [];
-$inputs= null;
-$heureDebut= null;
-$heureFin= null;
-echo "Veuillez saisir un creneaux \n";
-while (!(int)$heureDebut && !(int)$heureFin) {
-    $inputs = readline('Heure de début s\'il vous plait : ');
-if((int)$inputs) $heureDebut = (int)$inputs;
-if ($heureFin) {
-    echo "heure fin";
+
+use function PHPSTORM_META\type;
+
+$tabCreneaux = [];
+while (true) {
+    $debut = (int)readline("Heure d'ouverture : ");
+    $fin = (int)readline("Heure de fermeture : ");
+    if ($debut < $fin && $debut !== 0 && $fin !== 0){
+        array_push($tabCreneaux, $debut, $fin);
+        $newCreneaux =  readline("Voulez vous saisir un nouveau crée ? o/n : ");
+        if ($newCreneaux !== 'o') {
+            foreach ($tabCreneaux as $key => $value) {
+                if ($key % 2 === 0) { 
+                    echo "L'heure d'ouverture est $value. L'heure de fermeture est : " . $tabCreneaux[$key+1] . "\n";
+                }
+            }
+            return;
+        }
+    }
 }
-}
+?>
