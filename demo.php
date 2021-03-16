7dc749ce174f4813b046fbcaab05a881bfc1c496
idembele70 <idembele70@gmail.com>
2021-03-16 21:01:33 +0100

    fonctions partie 1

--- demo.php
+++ demo.php
@@ -1,25 +1,4 @@
 <?php
-$creneaux = [];
+$notes = [10, 20, 13];
 
-while (true) {
-    $debut = (int)readline('Heure d\'ouverture : ');
-    $fin = (int)readline('Heure de fermuture : ');
-    if ($debut >= $fin) {
-        echo 'Le créneaux ne peut pas etre enregistré car l\'heure dé' . $debut .'est supérieur à l\'heure de fermeture' . "\n";
-    } else {
-        $creneaux[] = [$debut, $fin];
-        $action = readline('Voulez vous enregistrer un nouveau créneau (o/n) : ');
-        if($action === 'n') {
-            break;
-        }
-    }
-}
-
-echo 'Le magasin est ouvert de';
-foreach ($creneaux as $k => $creneau) {
-    // echo $creneau[0] . 'h à' . $creneau[1] . 'h \n'; 
-    if ($k > 0) {
-        echo ' et de ';
-    }
-    echo " {$creneau[0]}h à {$creneau[1]}h";
-}
+echo round(array_sum($notes) / count($notes),1);
